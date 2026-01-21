50c68c41d3d0e17cb2ff2f3be080bafc79f78332
ikk12 <x9353953@gmail.com>
2026-01-21 14:54:01 +0800

    Update NotesModal.tsx

--- 拼图ultimate-(react版) (2)/components/NotesModal.tsx
+++ 拼图ultimate-(react版) (2)/components/NotesModal.tsx
@@ -17,13 +17,13 @@ export const NotesModal: React.FC<Props> = ({ onClose, onPermanentClose }) => {
                     <h3 className="text-[18px] font-bold text-gray-900">使用须知</h3>
                 </div>
                 <div className="text-[14px] text-gray-600 leading-relaxed mb-6 space-y-3 max-h-[60vh] overflow-y-auto pr-1">
-                    <p>1. 建议使用 <b>Edge</b> 浏览器以获得最佳体验。</p>
-                    <p>2. 如果图片超过 100 张，生成过程可能会有短暂卡顿。甚至第一组拼图是全黑色，如果遇到重新拼图。</p>
-                    <p>3. 多组图片导出，受浏览器影响，可能不会全部下载完图片。请尝试使用“打包下载(ZIP)”功能。</p>
-                    <p>4. ❗️❗️❗️多图一定要调一下画质，不然图片太大了❗️调30%也不影响看图❗️❗️❗️</p>
-                    <p>5. 老师们有时间可以找平替一些原因我可能会删链接，，，:0❗️</p>
-                    <p>6. ❗️超过一百张图不会合并生成一张拼图， 请自行填写行列，分多张图导出❗️</p>
-                    <p>7. 可在Edge浏览器选择 <b>添加到手机</b> 安装到主屏幕，不用再点链接打开</p>
+                    <p>1. 使用 <b>Edge</b> 浏览器。不同浏览器有不同问题，在edg目前我使用不会卡顿。有些浏览器不能多图导入。目前只用过edg浏览器</p>
+                    <p>2. 这个版本可拼一百张以上图合成一张图 不过要设置行列。比如200图，你要合成一张长图，就要设置15*15</p>
+                    
+                    <p>3. ❗️❗️❗️多图一定要调一下画质，不然图片太大了❗️调30%也不影响看图❗️❗️❗️</p>
+                    <p>4.禁止二传，不要什么都想着分享❗️</p>
+                    
+                    <p>5. 可在Edge浏览器选择 <b>添加到手机</b> 安装到主屏幕，不用再点链接打开</p>
                 </div>
                 <div className="flex items-center gap-3 mt-2">
                     <button onClick={onPermanentClose} className="text-xs text-gray-400 font-medium py-2 px-2 active:text-gray-600 transition">不再显示</button>
